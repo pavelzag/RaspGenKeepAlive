5aae9483cfef92943622bccea48ea07f5dffa1ec
pavelzagalsky <zagalsky@gmail.com>
2017-12-18 00:36:37 +0200

    Cosmetics

--- cloud_checker.py
+++ cloud_checker.py
@@ -7,8 +7,8 @@ from send_mail import send_mail
 
 failure = False
 sleep_time = 10
-fail_sleep_time = 2
-keep_alive_threshold = 12
+fail_sleep_time = 300
+keep_alive_threshold = 2
 owner = get_config('owner')
 
 
@@ -18,13 +18,12 @@ def check_time_delta(keep_alive_ts):
     return datetime.strptime(s2, time_format) - datetime.strptime(keep_alive_ts, time_format)
 
 if __name__ == '__main__':
-    send_mail(send_to=owner, subject='Start up Message', text='app is going up')
+    send_mail(send_to=owner, subject='Start up Message', text='Cloud checker app is going up')
     while 1 == 1:
         keep_alive_ts = get_keep_alive()['time_stamp']
         time_delta = check_time_delta(keep_alive_ts)
         logging_handler(str(time_delta))
         if time_delta > timedelta(minutes=keep_alive_threshold):
-            sleep_time = 300
             failure = True
             msg = '{} {} {}'.format('No keep alive for over', keep_alive_threshold, 'minutes')
             logging_handler(msg)
